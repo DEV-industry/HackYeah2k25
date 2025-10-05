cd6b96a086ef508ae1b8b3c03498d8541f2dccf1
Radosław Marek <dotims054@gmail.com>
2025-10-05 02:40:48 +0200

    little changes

--- ZUSim/client/src/components/DashboardTiles.tsx
+++ ZUSim/client/src/components/DashboardTiles.tsx
@@ -108,7 +108,7 @@ export default function DashboardTiles({
   };
 
 return (
-    <div className="relative max-w-[1100px] mx-auto mt-8 p-[65px]">
+    <div className="relative max-w-[1100px] mx-auto px-[65px] mt=[10px !important] mb-12">
       {!dashboardReady && (
         <div className="absolute -inset-5 bg-background/80 backdrop-blur-md rounded-xl z-10 flex items-center justify-center p-6">
           <p className="text-lg font-bold text-chart-1 text-center max-w-md">
